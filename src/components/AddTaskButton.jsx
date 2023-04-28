c77949598fe248305f857db85bad05e4c781a378
RiteshAdwani <ritesh.t@simformsolutions.com>
2023-04-28 12:03:27 +0530

    feat(WEB-12):add source code for todo-app's UI

---
+++ src/components/AddTaskButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddTaskButton extends React.Component {
+  render() {
+    return (
+      <div className="mt-5">
+        <button className="add-button">+</button>
+      </div>
+    );
+  }
+}
+
+export default AddTaskButton;
---
+++ src/data/ListData.jsx
@@ -0,0 +1,44 @@
+const listData = [
+  {
+    id: 1,
+    text: 'Buy a new sweatshirt',
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    text: 'Begin promotional phase',
+    isCompleted: true,
+  },
+  {
+    id: 3,
+    text: 'Read an article',
+    isCompleted: false,
+  },
+  {
+    id: 4,
+    text: 'Try not to fall asleep',
+    isCompleted: false,
+  },
+  {
+    id: 5,
+    text: 'Watch Sherlock',
+    isCompleted: false,
+  },
+  {
+    id: 6,
+    text: 'Begin QA product',
+    isCompleted: false,
+  },
+  {
+    id: 7,
+    text: 'Go for a walk',
+    isCompleted: false,
+  },
+  {
+    id: 8,
+    text: 'Take bike to service center',
+    isCompleted: false,
+  },
+];
+
+export default listData;
